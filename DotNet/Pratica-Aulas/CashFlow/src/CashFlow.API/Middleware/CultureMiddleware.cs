8592cfff6b15ff7dbc80c9dd0ff70588672b230e
Jefté Martins <jefte.martins@hotmail.com>
2025-10-18 15:23:45 -0300

    adicionando middleware de localização

---
+++ DotNet/Pratica-Aulas/CashFlow/src/CashFlow.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace CashFlow.API.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures)
+            //.Select(c => c.Name)
+            .ToList();
+        var requestedCulture = context.Request.Headers.AcceptLanguage.ToString();
+
+
+        if (!string.IsNullOrWhiteSpace(requestedCulture)
+            && supportedLanguages.Exists(l => l.Name.Equals(requestedCulture)))
+        {
+            var culture = new CultureInfo(requestedCulture);
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+        }
+        await _next(context);
+    }
+}
